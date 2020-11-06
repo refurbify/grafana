370f979b1a1f37e5851073dee83800bc457c8c0c
Dafydd <dafydd.thomas@grafana.com>
2020-11-06 11:20:20 +0000

    WIP * Notes on the state of working with API Keys against Hosted Grafana instances  *  * Key difference to the instances these tests are normally run against: Admin  * users do not seem to have the permissions to create new "orgs" in a HG instance.  * There fore the createTestOrgIfNotExists call fails, even if validating with an  * Admin API key.  *  * Current state:  *  * - This will run against a HG instance if you give a valid API key as the token  *   env.  * - The createTestOrg function will not error correctly if it gets a 401/403. If  *    this works correctly, it blocks the rest of the work.  * - createTestDatasource has been amended so it errors on e.g. a 403.  * - client setup moved into the default function - you cannot amend things in  *  the init state from the setup function.  *  * Actions:  * - Make the bearer auth option available in all tests, OR make a separate test  *  for it .  * - Amend the createOrg step so it errors correctly and/or doesnt run when  *  running against a HG instance

--- devenv/docker/loadtest/auth_token_test.js
+++ devenv/docker/loadtest/auth_token_test.js
@@ -2,6 +2,32 @@ import { sleep, check, group } from 'k6';
 import { createClient, createBasicAuthClient, createBearerAuthClient } from './modules/client.js';
 import { createTestOrgIfNotExists, createTestdataDatasourceIfNotExists } from './modules/util.js';
 
+/**
+ * Dafydd - 6 november 2020
+ * Notes on the state of working with API Keys against Hosted Grafana instances
+ * 
+ * Key difference to the instances these tests are normally run against: Admin
+ * users do not seem to have the permissions to create new "orgs" in a HG instance.
+ * There fore the createTestOrgIfNotExists call fails, even if validating with an
+ * Admin API key.
+ * 
+ * Current state: 
+ * 
+ * - This will run against a HG instance if you give a valid API key as the token
+ *   env.
+ * - The createTestOrg function will not error correctly if it gets a 401/403. If 
+ *    this works correctly, it blocks the rest of the work.
+ * - createTestDatasource has been amended so it errors on e.g. a 403.
+ * - client setup moved into the default function - you cannot amend things in
+ *  the init state from the setup function.
+ * 
+ * Actions:
+ * - Make the bearer auth option available in all tests, OR make a separate test
+ *  for it .
+ * - Amend the createOrg step so it errors correctly and/or doesnt run when
+ *  running against a HG instance
+ */
+
 export let options = {
   noCookiesReset: true,
 };
